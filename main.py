68f7adb31abef1d3234b72b2c3fef5ec076d9d68
ctrgy <christinetraan@gmail.com>
2025-08-14 19:51:30 -0400

    Update main.py

--- main.py
+++ main.py
@@ -144,6 +144,17 @@ hide_sidebar_style = """
         [data-testid="collapsedControl"] {
             display: none !important;
         }
+
+        /* This hides the collapse/expand sidebar button */
+        [data-testid="stSidebarNavCollapse"] {
+            display: none !important;
+        }
+
+        /* This hides the small chevron button in some Streamlit versions */
+        button[kind="header"] {
+            display: none !important;
+        }
+
     </style>
 """
 st.markdown(hide_sidebar_style, unsafe_allow_html=True)
